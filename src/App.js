2422d043e882a95bb45d70b13a354824e2369033
eisenils <mail.eisenils@gmail.com>
2020-04-18 20:54:43 +0200

    First implementation of Gallery

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Gallery from "react-photo-gallery";
+import { photos } from "./photos";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Schweinberg leuchtet</h1>
+      <Gallery photos={photos} />
+      <div>
+        &copy; {new Date().getFullYear()} NE Design |{" "}
+        <a href="http://www.nils-eisenhauer.de/impressum">Impressum</a>
+      </div>
     </div>
   );
 }

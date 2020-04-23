2f02762e3541b01325286701350379a560b140cb
eisenils <mail.eisenils@gmail.com>
2020-04-23 21:27:07 +0200

    First approach to implement full heigth header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from "react";
-import "./App.css";
-import Gallery from "react-photo-gallery";
-import { photos } from "./photos";
+import Hero from "./components/Hero";
+// import Gallery from "react-photo-gallery";
+// import { photos } from "./photos";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="neon m-5">
-        Schweinberg <span style={{ color: "#e92efb" }}>leuchtet</span>
-      </h1>
-      <Gallery photos={photos} />
+    <>
+      <Hero />
+      {/* <Gallery photos={photos} /> */}
       <div className="text-center">
         &copy; {new Date().getFullYear()} NE Design |{" "}
         <a href="http://www.nils-eisenhauer.de/impressum">Impressum</a>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Hero.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import FullHeight from "react-full-height";
+import video from "../hero.mp4";
+
+function Hero() {
+  return (
+    <>
+      <FullHeight className="hero-mobile d-md-none">
+        {/* <div className="d-md-none hero-image" /> */}
+        <div className="hero-text">
+          <h1 className="neon m-5">
+            Schweinberg <span style={{ color: "#e92efb" }}>leuchtet</span>
+          </h1>
+          <p>powered by Get Your Sound</p>
+        </div>
+      </FullHeight>
+      <FullHeight className=" d-none d-md-block parent">
+        <video autoPlay="autoplay" loop="loop" muted id="video">
+          <source src={video} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="hero-text">
+          <h1 className="neon m-5">
+            Schweinberg <span style={{ color: "#e92efb" }}>leuchtet</span>
+          </h1>
+          <p>powered by Get Your Sound</p>
+        </div>
+      </FullHeight>
+      {/* <div className="d-none d-md-block">
+        <video autoPlay="autoplay" loop="loop" muted id="video">
+          <source src={video} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div> */}
+    </>
+  );
+}
+
+export default Hero;

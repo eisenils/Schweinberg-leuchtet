43e7c9d0f9c2d06686dcfc29fa6c7381ce0ad5b8
eisenils <mail.eisenils@gmail.com>
2020-04-26 14:48:02 +0200

    Media changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import FullHeight from "react-full-height";
-import video from "../hero.mp4";
+import video from "../images/hero.mp4";
 
 function Hero() {
   return (
     <>
       <FullHeight className="hero-mobile d-md-none">
-        {/* <div className="d-md-none hero-image" /> */}
         <div className="hero-text">
           <h1 className="neon m-5">
             Schweinberg <span style={{ color: "#e92efb" }}>leuchtet</span>
           </h1>
-          <h3>powered by Get Your Sound</h3>
+          <h3 className="subheader-text">
+            <span style={{ fontSize: "1.3rem" }}>powered by</span>
+            <br /> <strong>GetYourSound</strong>
+          </h3>
         </div>
       </FullHeight>
       <FullHeight className=" d-none d-md-block">
@@ -20,10 +22,13 @@ function Hero() {
           Your browser does not support the video tag.
         </video>
         <div className="hero-text">
-          <h1 className="neon m-5">
+          <h1 className="neon m-5" style={{ fontSize: "3.5rem" }}>
             Schweinberg <span style={{ color: "#e92efb" }}>leuchtet</span>
           </h1>
-          <h3 className="subheader-text">powered by Get Your Sound</h3>
+          <h3 className="subheader-text">
+            <span style={{ fontSize: "1.3rem" }}>powered by</span>
+            <strong className="ml-2">GetYourSound</strong>
+          </h3>
         </div>
       </FullHeight>
     </>

1c3437a7f1032af8a8adf6e20aaef08a3d2782f4
eisenils <mail.eisenils@gmail.com>
2020-04-27 17:34:30 +0200

    Add Video

--- src/components/Impressions.jsx
+++ src/components/Impressions.jsx
@@ -2,6 +2,16 @@ import React from "react";
 
 import Gallery from "react-photo-gallery";
 import { photos } from "./photos";
+import video from "../images/hero.mp4";
+import {
+  Player,
+  ControlBar,
+  BigPlayButton,
+  CurrentTimeDisplay,
+  TimeDivider,
+  LoadingSpinner,
+  VolumeMenuButton,
+} from "video-react";
 
 function Impressions() {
   return (
@@ -14,6 +24,23 @@ function Impressions() {
         </div>
       </div>
       <Gallery photos={photos} />
+      <div className="container mt-3">
+        <div className="row">
+          <div className="col-8 offset-2">
+            <Player poster="/assets/poster.png">
+              <source src={video} />
+              <LoadingSpinner />
+
+              <BigPlayButton position="center" />
+              <ControlBar>
+                <CurrentTimeDisplay order={4.1} />
+                <TimeDivider order={4.2} />
+                <VolumeMenuButton />
+              </ControlBar>
+            </Player>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
